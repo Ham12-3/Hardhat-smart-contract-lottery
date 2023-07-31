da83601391e6a2e48f93b37b60efad8b359f1551
Ham12-3 <mobolaji2309@gmail.com>
2023-07-31 15:20:46 +0100

    committed

---
+++ hardhat.config.js
@@ -0,0 +1,57 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const COINMARKETCAP_API_KEY =
+    process.env.COINMARKETCAP_API_KEY || "2e6331c4-187c-41b8-bac2-9f500f47ff37"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "EHFJFZA7HBAZUKAVPJG716GBY49NVMJCW2"
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY || "0x5ce7066df10c9dc46da52be651f9b97ad268f1f6e20ec63a2560f5406f748001"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        sepolia: {
+            chainId: 11155111,
+            blockConfirmations: 6,
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: "0.8.8",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    mocha: {
+        timeout: 300000,
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "goerli",
+                chainId: 5,
+                urls: {
+                    apiURL: "https://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io",
+                },
+            },
+        ],
+    },
+}
